af5352f5c0235759e84903f361310a815c2ab784
heritaco <154515410+heritaco@users.noreply.github.com>
2024-04-30 21:27:29 -0600

    no

--- Ponchito/Ponchito Java/Ponchito.java
+++ Ponchito/Ponchito Java/Ponchito.java
@@ -1,43 +1,23 @@
-
 import java.sql.*;
-import java.io.*;
 
 public class Ponchito {
 
 	Connection conn = null;
 	Statement stmt = null;
-	BufferedReader in = null;
 
 	static final String URL = "jdbc:mysql://localhost/";
-	static final String BD = "ponchito"; // especificar: el nombre de la BD,
-	static final String USER = "root"; // el nombre de usuario
-	static final String PASSWD = "1234";// el password del usuario
+	static final String BD = "ponchito";
+	static final String USER = "root";
+	static final String PASSWD = "1234";
 
 	public Ponchito() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			System.out.print("Connecting to the database... ");
 			conn = DriverManager.getConnection(URL + BD, USER, PASSWD);
-			System.out.println("connected\n\n");
 			conn.setAutoCommit(false);
 			stmt = conn.createStatement();
-			in = new BufferedReader(new InputStreamReader(System.in));
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
-		}
-	}
-
-	private void dumpResultSet(ResultSet rset) throws SQLException {
-
-		ResultSetMetaData rsetmd = rset.getMetaData();
-		int i = rsetmd.getColumnCount();
-
-		while (rset.next()) {
-
-			for (int j = 1; j <= i; j++) {
-				System.out.print(rset.getString(j) + "\t");
-			}
-			System.out.println();
 		}
 	}
 
@@ -62,118 +42,6 @@ public class Ponchito {
 		conn.close();
 	}
 
-	private boolean menu() throws SQLException, IOException {
-
-		String statement;
-
-		System.out.println("\nNivel de aislamiento = " + conn.getTransactionIsolation() + "\n");
-		System.out.println("(1) Lista completa lugares a visitar\n");
-		System.out.println("(2) Lista restringida\n");
-		System.out.println("(3) Insertar un empleado\n");
-		System.out.println("(4) Borrar un empleado\n");
-		System.out.println("(5) Modificar el salario de un empleado\n");
-		System.out.println("(6) Validar todas operaciones\n");
-		System.out.println("(7) Abortar todas las operaciones\n");
-		System.out.println("(8) Cambiar nivel de aislamiento\n");
-		System.out.println("(9) Salir\n\n");
-		System.out.print("Option: ");
-
-		switch (Integer.parseInt("0" + in.readLine())) {
-
-			case 1:
-				query("select * from lugaravisitar");
-				break;
-
-			case 2:
-				System.out.println("\nPredicado?");
-				query("select * from lugaravisitar where " + in.readLine());
-				break;
-
-			case 3:
-				statement = "insert into lugaravisitar values ( ";
-
-				System.out.println("\nNumero?");
-				statement += in.readLine();
-
-				System.out.println("Nombre?");
-				statement += ", '" + in.readLine() + "', 'SALESMAN', ";
-
-				System.out.println("Jefe?");
-				statement += in.readLine() + ", '12-NOV-05', 1250.00,  500.00, 40 )";
-
-				stmt.executeUpdate(statement);
-				break;
-
-			case 4:
-				System.out.println("\nNombre?");
-				stmt.executeUpdate("delete from emp where ename = '" + in.readLine() + "'");
-				break;
-
-			case 5:
-				statement = "update lugaravisitar set ";
-
-				System.out.println("\n<Atributo> = <Valor>?");
-				statement += in.readLine();
-
-				System.out.println("Predicado?");
-				statement += " where " + in.readLine();
-
-				stmt.executeUpdate(statement);
-				break;
-
-			case 6:
-				conn.commit(); // fin de la transacción e inicio de la siguiente
-				break;
-
-			case 7:
-				conn.rollback(); // fin de la transacción e inicio de la siguiente
-				break;
-
-			case 8:
-				System.out.println();
-
-				System.out.println(conn.TRANSACTION_NONE + " = TRANSACTION_NONE");
-				System.out.println(conn.TRANSACTION_READ_UNCOMMITTED + " = TRANSACTION_READ_UNCOMMITTED");
-				System.out.println(conn.TRANSACTION_READ_COMMITTED + " = TRANSACTION_READ_COMMITTED");
-				System.out.println(conn.TRANSACTION_REPEATABLE_READ + " = TRANSACTION_REPEATABLE_READ");
-				System.out.println(conn.TRANSACTION_SERIALIZABLE + " = TRANSACTION_SERIALIZABLE\n\n");
-
-				System.out.println("Nivel?");
-				conn.setTransactionIsolation(Integer.parseInt(in.readLine()));
-				break;
-
-			case 9:
-				return false;
-		}
-
-		return true;
-	}
-
-	public static void main(String arg[]) throws SQLException, Exception {
-
-		if (arg.length != 0) {
-
-			System.err.println("Use: java Ponchito");
-			System.exit(1);
-		}
-
-		Ponchito transaction = new Ponchito();
-
-		while (true)
-
-			try {
-				if (!transaction.menu())
-					break;
-
-			} catch (Exception e) {
-
-				System.err.println("failed");
-				e.printStackTrace(System.err);
-			}
-
-		transaction.close();
-	}
-
 	public boolean addClient(String nombre, String apellidoPaterno, String apellidoMaterno,
 			String tipo, boolean agenciaEmpleado, int añoRegistro) {
 		String sql = "INSERT INTO Cliente (nombre, apellidoPaterno, apellidoMaterno, tipo, agenciaEmpleado, añoRegistro) VALUES (?, ?, ?, ?, ?, ?)";
@@ -186,6 +54,43 @@ public class Ponchito {
 			pstmt.setBoolean(5, agenciaEmpleado);
 			pstmt.setInt(6, añoRegistro);
 			int rowsAffected = pstmt.executeUpdate();
+			conn.commit(); // Commit the transaction
+			return rowsAffected > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean addReserva(Integer idcliente, String circuito) {
+		String sql = "INSERT INTO Cliente (idcliente, circuito) VALUES (?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, idcliente);
+			pstmt.setString(2, circuito);
+			int rowsAffected = pstmt.executeUpdate();
+			conn.commit(); // Commit the transaction
+			return rowsAffected > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean updateClient(int idCliente, String nombre, String apellidoPaterno, String apellidoMaterno,
+			String tipo, boolean agenciaEmpleado, int añoRegistro) {
+		String sql = "UPDATE Cliente SET nombre = ?, apellidoPaterno = ?, apellidoMaterno = ?, tipo = ?, agenciaEmpleado = ?, añoRegistro = ? WHERE idCliente = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, nombre);
+			pstmt.setString(2, apellidoPaterno);
+			pstmt.setString(3, apellidoMaterno);
+			pstmt.setString(4, tipo);
+			pstmt.setBoolean(5, agenciaEmpleado);
+			pstmt.setInt(6, añoRegistro);
+			pstmt.setInt(7, idCliente);
+			int rowsAffected = pstmt.executeUpdate();
+			conn.commit(); // Commit the transaction
 			return rowsAffected > 0;
 		} catch (SQLException e) {
 			e.printStackTrace();
--- Ponchito/Ponchito Java/PonchitoGUI.java
+++ Ponchito/Ponchito Java/PonchitoGUI.java
@@ -9,10 +9,9 @@ public class PonchitoGUI {
     private JTextArea textArea;
     private Ponchito ponchito;
     private JLabel welcomeLabel;
-
-    private JButton adminButton;
     private JDialog newUserDialog;
-    private JTextField nombreField, apellidopField, apellidomField, tipo, agenciaEmpleado, añoRegistro;
+    private JTextField nombreField, apellidopField, apellidomField, añoRegistro;
+    private JButton adminButton;
     private String adminPassword = "admin123"; // You should store passwords securely, not like this
 
     public PonchitoGUI() {
@@ -24,7 +23,7 @@ public class PonchitoGUI {
         JScrollPane scrollPane = new JScrollPane(textArea);
         panel1.add(scrollPane);
 
-        welcomeLabel = new JLabel("Welcome to Ponchito Viajes!!");
+        welcomeLabel = new JLabel("Bienvenido a Ponchito Viajes!!");
         panel1.add(welcomeLabel, BorderLayout.NORTH);
 
         try {
@@ -51,15 +50,191 @@ public class PonchitoGUI {
         adminButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String password = JOptionPane.showInputDialog("Enter password");
+                String password = JOptionPane.showInputDialog("Enter admin password:");
                 if (adminPassword.equals(password)) {
-                    openNewUserDialog();
+                    new AdminGUI().setVisible(true);
                 } else {
-                    JOptionPane.showMessageDialog(null, "Incorrect password");
+                    JOptionPane.showMessageDialog(null, "Incorrect password.");
                 }
             }
         });
         panel1.add(adminButton);
+    }
+
+    class AdminGUI extends JFrame {
+        public AdminGUI() {
+            setSize(300, 200);
+            setLayout(new FlowLayout());
+
+            JButton createUserButton = new JButton("Crear Cliente");
+            createUserButton.addActionListener(e -> CreateUser());
+            add(createUserButton);
+
+            JButton ReserButton = new JButton("Reservar");
+            ReserButton.addActionListener(e -> CreateReserva());
+            add(ReserButton);
+
+            JButton SeeClientsButton = new JButton("Ver Clientes");
+            SeeClientsButton.addActionListener(e -> VerClientes());
+            add(SeeClientsButton);
+
+            JButton VereservasButton = new JButton("Ver Reservas");
+            VereservasButton.addActionListener(e -> VerReservas());
+            add(VereservasButton);
+
+            JButton EditClientButton = new JButton("Editar Clientes");
+            EditClientButton.addActionListener(e -> EditClient());
+            add(EditClientButton);
+        }
+
+        private void CreateUser() {
+            newUserDialog = new JDialog();
+            newUserDialog.setTitle("Nuevo Cliente");
+            newUserDialog.setLayout(new GridLayout(7, 2));
+
+            nombreField = addField(newUserDialog, "Nombre:");
+            apellidopField = addField(newUserDialog, "Apellido Paterno:");
+            apellidomField = addField(newUserDialog, "Apellido Materno:");
+
+            String[] tipos = { "compañía", "grupo", "individual" };
+            JComboBox<String> tipo = new JComboBox<>(tipos);
+            newUserDialog.add(new JLabel("Tipo:"));
+            newUserDialog.add(tipo);
+
+            String[] booleanOptions = { "true", "false" };
+            JComboBox<String> agenciaEmpleado = new JComboBox<>(booleanOptions);
+            newUserDialog.add(new JLabel("Agencia Empleado:"));
+            newUserDialog.add(agenciaEmpleado);
+
+            añoRegistro = addField(newUserDialog, "Año de registro:");
+
+            JButton saveButton = new JButton("Save");
+            saveButton.addActionListener(e -> {
+                try {
+                    boolean agenciaEmpleadoBool = Boolean.parseBoolean((String) agenciaEmpleado.getSelectedItem());
+
+                    String tipoSelected = (String) tipo.getSelectedItem();
+
+                    boolean clientAdded = ponchito.addClient(
+                            nombreField.getText(),
+                            apellidopField.getText(),
+                            apellidomField.getText(),
+                            tipoSelected,
+                            agenciaEmpleadoBool,
+                            Integer.parseInt(añoRegistro.getText()));
+
+                    if (clientAdded) {
+                        JOptionPane.showMessageDialog(null, "Cliente añadido correctamente!");
+                    } else {
+                        JOptionPane.showMessageDialog(null,
+                                "Ha ocurrido un error al añadir el cliente. Por favor, inténtelo de nuevo.");
+                    }
+
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Por favor, introduzca un año de registro válido.");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+
+            newUserDialog.add(saveButton);
+            newUserDialog.pack();
+            newUserDialog.setVisible(true);
+        }
+
+        private void CreateReserva() {
+            JDialog reservaDialog = new JDialog();
+            reservaDialog.setTitle("Nueva Reserva");
+            reservaDialog.setLayout(new GridLayout(5, 2));
+
+            JTextField clienteField = addField(reservaDialog, "Cliente:");
+            JTextField circuitoField = addField(reservaDialog, "Circuito:");
+
+            JButton saveButton = new JButton("Save");
+            saveButton.addActionListener(e -> {
+                try {
+                    boolean reservaAdded = ponchito.addReserva(
+                            Integer.parseInt(clienteField.getText()), //
+                            circuitoField.getText());
+
+                    if (reservaAdded) {
+                        JOptionPane.showMessageDialog(null, "Reserva añadida correctamente!");
+                    } else {
+                        JOptionPane.showMessageDialog(null,
+                                "Ha ocurrido un error al añadir la reserva. Por favor, inténtelo de nuevo.");
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+
+            reservaDialog.add(saveButton);
+            reservaDialog.pack();
+            reservaDialog.setVisible(true);
+        }
+
+        private void VerClientes() {
+            try {
+                String results = ponchito.query("select * from cliente");
+                textArea.setText(results);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        private void VerReservas() {
+            try {
+                String results = ponchito.query("select * from reserva");
+                textArea.setText(results);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        private void EditClient() {
+            JDialog editClientDialog = new JDialog();
+            editClientDialog.setTitle("Editar Cliente");
+            editClientDialog.setLayout(new GridLayout(8, 2));
+
+            JTextField idField = addField(editClientDialog, "ID del cliente:");
+            JTextField nombreField = addField(editClientDialog, "Nombre:");
+            JTextField apellidoPaternoField = addField(editClientDialog, "Apellido Paterno:");
+            JTextField apellidoMaternoField = addField(editClientDialog, "Apellido Materno:");
+            String[] tipos = { "compañía", "grupo", "individual" };
+            JComboBox<String> tipoField = new JComboBox<>(tipos);
+            editClientDialog.add(new JLabel("Tipo:"));
+            editClientDialog.add(tipoField);
+            JCheckBox agenciaEmpleadoField = new JCheckBox("Agencia Empleado");
+            editClientDialog.add(agenciaEmpleadoField);
+            JTextField añoRegistroField = addField(editClientDialog, "Año de registro:");
+
+            JButton saveButton = new JButton("Save");
+            saveButton.addActionListener(e -> {
+                try {
+                    boolean clientUpdated = ponchito.updateClient(
+                            Integer.parseInt(idField.getText()),
+                            nombreField.getText(),
+                            apellidoPaternoField.getText(),
+                            apellidoMaternoField.getText(),
+                            (String) tipoField.getSelectedItem(),
+                            agenciaEmpleadoField.isSelected(),
+                            Integer.parseInt(añoRegistroField.getText()));
+                    if (clientUpdated) {
+                        JOptionPane.showMessageDialog(editClientDialog, "Cliente editado!");
+                    } else {
+                        JOptionPane.showMessageDialog(editClientDialog, "Hay algo mal.", "Error",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(editClientDialog, "Hay algo mal.", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            });
+            editClientDialog.add(saveButton);
+            editClientDialog.pack();
+            editClientDialog.setVisible(true);
+        }
     }
 
     private JTextField addField(JDialog dialog, String labelText) {
@@ -67,62 +242,6 @@ public class PonchitoGUI {
         JTextField field = new JTextField();
         dialog.add(field);
         return field;
-    }
-
-    private void openNewUserDialog() {
-        newUserDialog = new JDialog();
-        newUserDialog.setTitle("New User");
-        newUserDialog.setLayout(new GridLayout(7, 2));
-
-        nombreField = addField(newUserDialog, "Nombre:");
-        apellidopField = addField(newUserDialog, "Apellido Paterno:");
-        apellidomField = addField(newUserDialog, "Apellido Materno:");
-
-        String[] tipos = { "compañía", "grupo", "individual" };
-        JComboBox<String> tipo = new JComboBox<>(tipos);
-        newUserDialog.add(new JLabel("Tipo:"));
-        newUserDialog.add(tipo);
-
-        // Use a JComboBox for agenciaEmpleado
-        String[] booleanOptions = { "true", "false" };
-        JComboBox<String> agenciaEmpleado = new JComboBox<>(booleanOptions);
-        newUserDialog.add(new JLabel("Agencia Empleado:"));
-        newUserDialog.add(agenciaEmpleado);
-
-        añoRegistro = addField(newUserDialog, "Año de registro:");
-
-        JButton saveButton = new JButton("Save");
-        saveButton.addActionListener(e -> {
-            try {
-                // Parse the selected option to a boolean
-                boolean agenciaEmpleadoBool = Boolean.parseBoolean((String) agenciaEmpleado.getSelectedItem());
-
-                String tipoSelected = (String) tipo.getSelectedItem();
-
-                boolean clientAdded = ponchito.addClient(
-                        nombreField.getText(),
-                        apellidopField.getText(),
-                        apellidomField.getText(),
-                        tipoSelected,
-                        agenciaEmpleadoBool,
-                        Integer.parseInt(añoRegistro.getText()));
-
-                if (clientAdded) {
-                    JOptionPane.showMessageDialog(null, "Client added successfully");
-                } else {
-                    JOptionPane.showMessageDialog(null, "An error occurred while adding the client to the database");
-                }
-
-            } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(null, "Please enter a valid number for 'Año de registro'");
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-
-        newUserDialog.add(saveButton);
-        newUserDialog.pack();
-        newUserDialog.setVisible(true);
     }
 
     private JButton createButton(String text, String query) {
